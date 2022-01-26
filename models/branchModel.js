77356e1e8b59ff5d4a158cad1b00da4036c8c190
fem_97 <fem.nwu@gmail.com>
2022-01-26 16:46:56 +0600

    updated

--- models/branchModel.js
+++ models/branchModel.js
@@ -5,14 +5,18 @@ const branchSchema = new mongoose.Schema({
     branchID: String,
     branchName: String,
     branchDesc: String,
-    store: {
-        storeID: String,
-        storeName: String,
-        bin: {
-            binID: String,
-            binName: String
+    store: [
+        {
+            storeID: String,
+            storeName: String,
+            bin: [
+                {
+                    binID: String,
+                    binName: String
+                }
+            ]
         }
-    }
+    ]
 })
 
 //we will create a new collection
--- routeHandler/adminRoute/adminRouter.js
+++ routeHandler/adminRoute/adminRouter.js
@@ -2,9 +2,50 @@ const express = require('express');
 const adminRouter = express.Router();
 const Branch = require('../../models/branchModel');
 
-adminRouter.get('/', async (req, res) => {
+// Admin Dashboard
+adminRouter.get('/dashboard', async (req, res) => {
     res.send("Admin dashboard");
 });
+
+adminRouter.put('/branch/:id', async (req, res) => {
+    try {
+        console.log("req body", req.body);
+        // const { storeID, storeName } = req.body;
+        console.log(req.params.id);
+        const result = await Branch.updateOne(
+            { _id: req.params.id },
+            { $push: { store: req.body } }
+        );
+
+        res.send(result);
+
+        // const updatedBranch = await Branch.updateOne({ _id: req.params.id }, {
+        //     $set: {
+        //         store: {
+        //             storeID: storeID,
+        //             storeName: storeName
+        //         }
+        //     }
+        // })
+
+        // UPDATE branch here
+        // const storeID = req.body.newStoreState.storeID;
+        // const storeName = req.body.newStoreState.storeName;
+        // const updatedBranch = await Todo.updateOne({ _id: req.params.id }, {
+        //     $set: {
+        //         store: {
+        //             storeID,
+        //             storeName
+        //         }
+        //     }
+        // })
+
+
+
+    } catch (error) {
+        res.send("Error")
+    }
+})
 
 // GET ALL Branches
 adminRouter.get('/branches', async (req, res) => {
@@ -15,6 +56,8 @@ adminRouter.get('/branches', async (req, res) => {
         res.status(500).json({ error });
     }
 })
+
+
 
 // POST a new branch
 adminRouter.post('/branch', async (req, res) => {
